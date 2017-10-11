4e83492a76dd80a577ef4cddd2ed7084c2a4ce57
Aaditya Kalsi <akalsi.dev@gmail.com>
2017-10-10 23:57:57 -0400

    Dont remove _build from install tests

--- tests/install/test.sh
+++ tests/install/test.sh
@@ -17,7 +17,7 @@ fi
 cmd="cmake -H. -B$currdir/_build $gen_arg -DCMAKE_INSTALL_PREFIX=$root/_install -DTESTS_DIR=$root/tests"
 sh -c "$cmd"
 env VERBOSE=1 cmake --build $currdir/_build --target tests_run > $currdir/testLog.txt
-rm -fr $currdir/_build
+# rm -fr $currdir/_build
 
 echo '----------------------------------------------------------'
 echo '# TEST LOG                                               #'
